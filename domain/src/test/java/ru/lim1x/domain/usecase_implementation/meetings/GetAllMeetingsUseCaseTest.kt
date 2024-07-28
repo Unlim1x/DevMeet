4bbe9ac707614c7a743796a55adec9b016d27563
Unlim1x <anton@kolodij.ru>
2024-07-28 15:37:02 +0300

    Добавил еще тестик, проверяющий, что приходит список встреч с разными id

---
+++ domain/src/test/java/ru/lim1x/domain/usecase_implementation/meetings/GetAllMeetingsUseCaseTest.kt
@@ -0,0 +1,29 @@
+package ru.lim1x.domain.usecase_implementation.meetings
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import ru.lim1x.domain.repository.DataSourceTest
+import ru.lim1x.domain.repository.MeetingRepositoryTest
+
+class GetAllMeetingsUseCaseTest{
+    private val meetingsRepository = MeetingRepositoryTest(DataSourceTest())
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Should return MeetingList with different ids`() = runTest {
+
+
+        val useCase = GetAllMeetingsUseCase(meetingRepository = meetingsRepository)
+        val result= useCase.execute().single()
+
+        val actual = result.groupingBy { it.id }.eachCount().filter { it.value > 1 }
+
+        advanceUntilIdle()
+        assertTrue(actual.isEmpty())
+    }
+}
